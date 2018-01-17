2eb8b031dfed5bf99c966cf2f5984ae6db9d0cf8
root <root@localhost.localdomain>
2018-01-17 14:09:54 +0300

    updates

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,10 +9,6 @@ log = logging.getLogger(__name__)
 
 @channel_session
 def ws_connect(message):
-    # Extract the room from the message. This expects message.path to be of the
-    # form /chat/{label}/, and finds a Room if the message path is applicable,
-    # and if the Room exists. Otherwise, bails (meaning this is a some othersort
-    # of websocket). So, this is effectively a version of _get_object_or_404.
     try:
         prefix, label = message['path'].decode('ascii').strip('/').split('/')
         if prefix != 'chat':
@@ -29,15 +25,12 @@ def ws_connect(message):
     log.debug('chat connect room=%s client=%s:%s', 
         room.label, message['client'][0], message['client'][1])
     
-    # Need to be explicit about the channel layer so that testability works
-    # This may be a FIXME?
     Group('chat-'+label, channel_layer=message.channel_layer).add(message.reply_channel)
 
     message.channel_session['room'] = room.label
 
 @channel_session
 def ws_receive(message):
-    # Look up the room from the channel session, bailing if it doesn't exist
     try:
         label = message.channel_session['room']
         room = Room.objects.get(label=label)
@@ -65,7 +58,6 @@ def ws_receive(message):
             room.label, data['handle'], data['message'])
         m = room.messages.create(**data)
 
-        # See above for the note about Group
         Group('chat-'+label, channel_layer=message.channel_layer).send({'text': json.dumps(m.as_dict())})
 
 @channel_session
---
+++ readme.txt
@@ -0,0 +1,10 @@
+This is a 
+
+to run it install the requirements in the (requirements.txt file)
+
+ensure to have redis-server running
+
+run the redis-server service then run python manager.py runserver to run the application.
+
+in your browser navigate to http://127.0.0.1:8000
+ 
